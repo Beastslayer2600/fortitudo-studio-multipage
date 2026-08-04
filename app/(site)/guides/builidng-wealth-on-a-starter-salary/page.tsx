ea146513b2c9616d4111d9b4db690f4b44292ba1
Beastslayer2600 <gertjiefourie@icloud.com>
2026-08-04 17:49:26 +0200

    Delete app/(site)/guides/builidng-wealth-on-a-starter-salary/page.tsx

--- app/(site)/guides/builidng-wealth-on-a-starter-salary/page.tsx
+++
@@ -1,5 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-
-export default function LegacyBuildingWealthOnAStarterSalaryPage() {
-  permanentRedirect("/guides/building-wealth-on-a-starter-salary");
-}
