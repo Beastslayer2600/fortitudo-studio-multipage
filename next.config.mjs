859f80f37aaa7005f60194a380c1684bf72f3bd6
Beastslayer2600 <gertjiefourie@icloud.com>
2026-08-04 17:31:53 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,2 +1,14 @@
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "fortitudostudios.site" }],
+        destination: "https://www.fortitudostudios.site/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 export default nextConfig;
